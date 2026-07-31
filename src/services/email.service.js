8e05f50c428d2b211640181306b1285ba1f14926
Coderuser135 <mdmehtab543211@gmail.com>
2026-07-31 17:05:01 +0530

    fix email transporter for render

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GOOGLE_USER,
     pass: process.env.GOOGLE_PASS,
